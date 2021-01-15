64d8c2fb82272479e63fb5c5bce7bdf07a46e65f
Allan Bernard R. De Guzman <abeydeguzman@gmail.com>
2021-01-15 17:56:06 +0800

    Placed variable values for mysqli() conn

--- dbconnection-fileupload/db_connection.php
+++ dbconnection-fileupload/db_connection.php
@@ -3,7 +3,7 @@
         //place database credentials here.
         $dbhost = "localhost";
         $dbuser = "root";
-        $dbpass = "";
+        $dbpass = ""; //maybe make an .env for the password for security
         $db = "central-portfolio"; // Name of our database. Make sure to use this name when creating your database on phpMyAdmin
         $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
